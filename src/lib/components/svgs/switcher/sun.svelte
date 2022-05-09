ed31507718df26e5451eed892b2265db7c06978e
nisarhassan12 <syednisarhassan12@gmail.com>
2022-05-09 19:45:36 +0500

    update the sun svg.

--- src/lib/components/svgs/switcher/sun.svelte
+++ src/lib/components/svgs/switcher/sun.svelte
@@ -1,14 +1,22 @@
-<svg viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+<svg
+  width="1140"
+  height="1140"
+  viewBox="0 0 1140 1140"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
   <path
-    d="M9 17C13.4183 17 17 13.4183 17 9C17 4.58172 13.4183 1 9 1C4.58172 1 1 4.58172 1 9C1 13.4183 4.58172 17 9 17Z"
+    d="M570 1076.67C849.824 1076.67 1076.67 849.824 1076.67 570C1076.67 290.176 849.824 63.333 570 63.333C290.176 63.333 63.333 290.176 63.333 570C63.333 849.824 290.176 1076.67 570 1076.67Z"
     class="stroke-secondary dark:stroke-light-black"
-    stroke-width="2"
+    stroke="#514F4D"
+    stroke-width="126.667"
     stroke-linecap="round"
     stroke-linejoin="round"
-    stroke-dasharray="0.01 6"
+    stroke-dasharray="0.63 360"
   />
   <path
-    d="M9 13C11.2091 13 13 11.2091 13 9C13 6.79086 11.2091 5 9 5C6.79086 5 5 6.79086 5 9C5 11.2091 6.79086 13 9 13Z"
+    d="M570 823.333C709.912 823.333 823.333 709.912 823.333 570C823.333 430.088 709.912 316.667 570 316.667C430.088 316.667 316.667 430.088 316.667 570C316.667 709.912 430.088 823.333 570 823.333Z"
+    fill="#514F4D"
     class="fill-secondary dark:fill-light-black"
   />
 </svg>
